a694b6f04c54ed52eba0a927990f1cdbe479cd3c
anipuni <punipunianime@gmail.com>
2015-03-01 13:33:32 +0900

    Lazy

---
+++ LazyLoad.js
@@ -0,0 +1,2 @@
+﻿jQuery(document).ready(function($){ if (navigator.platform == &quot;iPad&quot;) return; jQuery(&quot;.post-body img&quot;).lazyload({ effect:&quot;fadeIn&quot;, placeholder: &quot;http:
+});});
