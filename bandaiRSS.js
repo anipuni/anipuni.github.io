fcc2446802515412c52e212a70dc22935407629d
anipuni <punipunianime@gmail.com>
2015-06-26 07:27:39 +0900

    bandai

--- bandaiRSS.js
+++ bandaiRSS.js
@@ -55,7 +55,7 @@ var dq = d.getMonth()+""+d.getDate()+""+d.getHours()+d.getMinutes();
    d = (d < 10)? "0" + d:d;//日数字を2桁に
    var date = Y + "年" + m + "月" + d + "日";
 
-   html += '<dd>■<a href="' + entry.link + '" target="_blank">' + entry.title + '</a>（' + date + '）';
+   html += '<dd>■<a href="' + entry.link + '" target="_blank">' + entry.title + '</a><br/>　　（' + date + '）';
    if(now >= entry.sortDate && now <= (entry.sortDate + pastTime)){
     html += '<strong style="color:red">new!</strong>';
    }
