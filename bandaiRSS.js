df6246afa644cf67deb53f675986cdd45b13dde7
anipuni <punipunianime@gmail.com>
2015-06-27 05:04:30 +0900

    bandaiRSS

--- bandaiRSS.js
+++ bandaiRSS.js
@@ -53,7 +53,7 @@ var dq = d.getMonth()+""+d.getDate()+""+d.getHours();
    m = (m < 10)? "0" + m:m;//月数字を2桁に
    var d = pdate.getDate();
    d = (d < 10)? "0" + d:d;//日数字を2桁に
-   var date = " " + m + "/" + d + "～";
+   var date = Y + "年" + m + "月" + d + "日";
 
    html += '<dd>■<a href="' + entry.link + '" target="_blank">' + entry.title + '</a>' + date + ';
    if(now >= entry.sortDate && now <= (entry.sortDate + pastTime)){
