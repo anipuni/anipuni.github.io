ddae024882d05ea7cf8a85ce712336c4406fe848
anipuni <punipunianime@gmail.com>
2015-07-05 06:11:30 +0900

    tab

---
+++ tabmobile.js
@@ -0,0 +1,1 @@
+﻿function ChangeTab(tabname) { document.getElementById('tab1').style.display = 'none'; document.getElementById('tab2').style.display = 'none'; document.getElementById('tab3').style.display = 'none'; document.getElementById('tab4').style.display = 'none'; document.getElementById(tabname).style.display = 'block';}
