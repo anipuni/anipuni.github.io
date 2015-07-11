bda9d73405a8bbe56911abe5d22ca348982ac8d7
anipuni <punipunianime@gmail.com>
2015-07-11 18:25:50 +0900

    スクロール

--- scrollBtn.js
+++ scrollBtn.js
@@ -1,3 +1,3 @@
-﻿$(function() { var topBtn = $('#page-top'); topBtn.hide(); $(window).scroll(function () { if ($(this).scrollTop() > 500) { topBtn.fadeIn();} else { topBtn.fadeOut();}
+﻿$(function() { var topBtn = $('#page-top'); topBtn.hide(); $(window).scroll(function () { if ($(this).scrollTop() > 10) { topBtn.fadeIn();} else { topBtn.fadeOut();}
 }); topBtn.click(function () { $('body,html').animate({ scrollTop: 0
-}, 500); return false;});}); 
+}, 10); return false;});}); 
