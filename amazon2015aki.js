7f44720f5978d0e13b200fbe4834ecc998c1065c
anipuni <punipunianime@gmail.com>
2015-10-27 13:12:11 +0900

    amazon2015aki

--- amazon2015aki.js
+++ amazon2015aki.js
@@ -1,75 +1,28 @@
-﻿google.load("feeds", "1");
-//<![CDATA[
-var getRssFeeds = function (_id, _urls, _maxLength) {
- if(!_id || !_urls || (!(_urls instanceof Array))) return;
- var entryNum = 20;//各RSSの読み込みエントリー数
- var maxLength = (_maxLength)?  _maxLength : 0 ;
- //総エントリー表示数（0はすべて表示)
- //変数定義
- var entriesArray = new Array();
- var complete = 0;
- 
- //初期化
- var init = function () {
-  for(var i=0 ; i < _urls.length ; i++){
-   //RSS読み込み
-var d = new Date();
-var dq = d.getMonth()+""+d.getDate()+""+d.getHours()+d.getMinutes();
-   var feed = new google.feeds.Feed(_urls[i]+"?"+dq);
-   feed.setNumEntries(entryNum);
-   feed.load(function(result) {
-    if (!result.error) {
-     for (var i = 0; i < result.feed.entries.length; i++) {
-      var entry = result.feed.entries[i];
-      entriesArray.push(entry);
-      var pdate = new Date(entry.publishedDate);
-      var arr = entriesArray[(entriesArray.length-1)];
-
-      arr.siteTitle = result.feed.title;
-     }
-    }
-    complete++;
-    if(_urls.length == complete) echo();
-   });
-  }
- };
- //表示
- var echo = function () {
-  var feedLength = (_maxLength)? _maxLength : entriesArray.length;
-  
-  var pastDay = 0;//newマークをつける日数
-  var now = (new Date()).getTime();
-  var pastTime = pastDay * 24 * 60 * 60 * 1000;
-  
-  var container = document.getElementById(_id);
-  var html='<dl>';
-  for (var i = 0; i < feedLength; i++) {
-   var entry = entriesArray[i];
-   var pdate = new Date(entry.publishedDate);
-   var Y = pdate.getFullYear();
-   var m = pdate.getMonth() + 1;
-   m = (m < 10)? "0" + m:m;//月数字を2桁に
-   var d = pdate.getDate();
-   d = (d < 10)? "0" + d:d;//日数字を2桁に
-   var date = Y + "年" + m + "月" + d + "日";
-   html += '<dd><a href="' + entry.link + '" target="_blank">' + entry.title + '</a>';
-   
-   if(now >= entry.sortDate && now <= (entry.sortDate + pastTime)){
-    html += '<strong style="color:red">new!</strong>';
-   }
-   
-   html += '</dd>';
-  }
-  html += '</dl>';
-  container.innerHTML += html;
- };
- 
- google.setOnLoadCallback(init);
-};
-
-
-getRssFeeds("amazon2015aki", [
-"http://www.amazon.co.jp/gp/rss/bestsellers/dvd/3932521051/ref=zg_bs_3932521051_rsslink&tag=anibull-22"
-]);
-
-//]]>
+﻿var readAmazonFeed = function(feedurl,selector,maxlength){
+    function initialize(){
+        var feed = new google.feeds.Feed(feedurl);
+        feed.setNumEntries(maxlength);
+        feed.load(function(result) {
+            if (!result.error) {
+                var dst = $("<ul>");
+                for (var i = 0; i < result.feed.entries.length; i++) {
+                    var entry = result.feed.entries[i];
+                    var title = entry.title;
+                    var link = entry.link;
+                         
+                    var content = entry.content;
+                    var matches = content.match(/http[s]?\:\/\/[\w\+\$\;\?\.\%\,\!\#\~\*\/\:\@\&\\\=\_\-]+(jpg|jpeg|
+ 
+gif|png|bmp)/g);
+                    var tmp = '&lt;li&gt;&lt;a href="' + link + '"&gt;&lt;img src="' + matches[0] + 
+ 
+'"/&gt;&lt;/a&gt;&lt;/li&gt;';
+                    dst.append(tmp);
+                }
+                $(selector).append(dst);
+            }
+        });
+    }
+    google.setOnLoadCallback(initialize);
+}
+readAmazonFeed("http://www.amazon.co.jp/gp/rss/bestsellers/dvd/3932521051/ref=zg_bs_3932521051_rsslink&tag=anibull-22","#amazon2015aki",10);
