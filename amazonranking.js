137f458b8ec7ae9bb49181db1f9c5010613e7a4e
anipuni <punipunianime@gmail.com>
2015-10-27 14:56:18 +0900

    amazonranking

--- amazonranking.js
+++ amazonranking.js
@@ -21,7 +21,7 @@
     					imgtag = "<img src='" + imgarray[0] + "' border='0'/>";
     				}
 
-    				jQuery('#amazonranking').append("<li style='list-style-type: none'><a href='" + link + "'>" + imgtag + "</a></li>"); //画像リンクを挿入
+    				jQuery('#amazonranking').append("<a href='" + link + "'>" + imgtag + "</a>"); //画像リンクを挿入
     			}
     		}
     	});
