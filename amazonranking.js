fc045eb071badac57fa651803125bb3968b5a2fb
anipuni <punipunianime@gmail.com>
2015-11-02 23:45:03 +0900

    amazonranking

--- amazonranking.js
+++ amazonranking.js
@@ -21,7 +21,7 @@
     					imgtag = "<img src='" + imgarray[0] + "' border='0'/>";
     				}
 
-    				jQuery('#amazonranking').append("<a href='" + link + "'>" + imgtag + "</a>"); //画像リンクを挿入
+    				jQuery('#amazonranking').append("<a href='" + link + "'target='_blank'>" + imgtag + "</a>"); //画像リンクを挿入
     			}
     		}
     	});
