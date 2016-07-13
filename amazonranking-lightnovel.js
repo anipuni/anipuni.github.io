eff4c6ccb11f9818cf48787ed8d24606d6ee4532
anipuni <punipunianime@gmail.com>
2016-07-13 14:36:16 +0900

    a

--- amazonranking-lightnovel.js
+++ amazonranking-lightnovel.js
@@ -23,7 +23,7 @@ var dq = d.getMonth()+""+d.getDate()+""+d.getHours();
     					imgtag = "<img src='" + imgarray[0] + "' border='0'/>";
     				}
 
-    				jQuery('#amazonranking-lightnovel').append("<div class='amaran'><div class='amarangazo'><a href='" + link + "'target='_blank' rel='nofollow'></div>" + imgtag + "<div class='amarantitle'>" + entry.title +"</div></div>"); //画像リンクを挿入
+    				jQuery('#amazonranking-lightnovel').append("<div class='amazonrannovel'><a href='" + link + "'target='_blank' rel='nofollow'>" + imgtag + "</a></div>"); //画像リンクを挿入
     			}
     		}
     	});
